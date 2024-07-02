f53d700552b399eecb14301440c469aba63b2bee
Ted Michael Celis <krazete@gmail.com>
2024-07-01 18:50:12 -0700

    trimmed output path and added comments

--- adbget.sh
+++ adbget.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 # retrieve all files with a last modified time of a week ago or less
 now=$(date +%s)
-filelist=$(adb shell ls -Rpgot sdcard/Android/data/com.YoStarEN.AzurLane/files/AssetBundles)
+filelist=$(adb shell ls -Rpgot sdcard/Android/data/com.YoStarEN.AzurLane/files/AssetBundles) # sort by and include last modified time (mtime)
 while IFS=$'\t\r\n' read -r line; do
     if [[ $line == *: ]]; then
-        parent=${line:0:-1} # parent folder
+        parent=${line:0:-1} # remember parent folder
     elif [[ $line == */ ]]; then
         continue # ignore subfolders
     elif [[ $parent != "" && $line =~ ([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2})" "(.+) ]]; then
-        # age=$(adb shell -n stat -c %Y $filename) # get last modified time in seconds of individual file
-        stamp=${BASH_REMATCH[1]} # date and time (1970-01-01 00:00)
+        stamp=${BASH_REMATCH[1]} # date and time; format: "1970-01-01 00:00"
         mtime=$(date -d "$stamp" +%s) # stamp in seconds
-        age=$(($now - $mtime - 7*86400)) # 1 day = 86400
+        # mtime=$(adb shell -n stat -c %Y $path) # unused alternative; too expensive and slow
+        age=$(($now - $mtime - 7*86400)) # date difference minus a week
         if [[ $age == -* ]]; then
-            name=${BASH_REMATCH[2]} # file name
-            path=$parent/$name # file path
-            mkdir -p $parent
-            adb pull $path $path
+            name=${BASH_REMATCH[2]}
+            path=$parent/$name # file path on phone
+            dstparent=${parent:48}
+            mkdir -p $dstparent # create folder on computer
+            dstpath=$dstparent/$name # file path for computer
+            adb pull -a $path $dstpath # copy file with mtime preserved
         else
-            parent="" # after one old file, ignore the rest of the list
+            parent="" # after encountering one old file, ignore the rest of the file list for the parent folder
         fi
     fi
 done <<< "$filelist"
