035c4ae6d1f7bf94b4cbc4fbfc18d7c55a8f9f12
Ted Michael Celis <krazete@gmail.com>
2024-08-18 22:28:13 -0700

    Update buildjuustagram.py

--- buildjuustagram.py
+++ buildjuustagram.py
@@ -1,6 +1,14 @@
-    dl_sharecfg('juustagram', ['CN', 'EN', 'JP'], [
-        'activity_ins_language',
-        'activity_ins_npc_template',
-        'activity_ins_ship_group_template',
-        'activity_ins_template'
-    ])
+from argparse import ArgumentParser
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('-d', '--download', action='store_true', help='download data files')
+    args = parser.parse_args()
+    if args.download:
+        from downloader import dl_sharecfg
+        dl_sharecfg('juustagram', ['CN', 'EN', 'JP'], [
+            'activity_ins_language',
+            'activity_ins_npc_template',
+            'activity_ins_ship_group_template',
+            'activity_ins_template'
+        ])
